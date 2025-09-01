457f43d41ce759db5868f6a039e6dbde470ed341
carter <carter@cartercoding.com>
2025-09-01 16:33:50 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+# retuner
+
+this is a very simple website that allows you to play using shasavic music theory thru webmidi.
+
+> [!WARNING]
+> this program is very new and likely has many bugs.
+> please make an issue for any bugs you see!
+
+## credits
+
+i wrote all the code and the fonts and music theory are made by LΛMPLIGHT.
+
+i also converted the lapisia font to woff (it was otf originally)
+
+## important info
+
+- this midi controller is monophonic, meaning it can only send one note at a time.
+- Ah is tuned to 289.56Hz, so it is in tune with Yxeni.
+- this program assumes your synth is tuned to A4=440Hz.
+- a lot of numbers are currently hard programmed. i'll add settings for them later though!
+
+for anything not mentioned here feel free to dm me on discord!
---
+++ script.js
@@ -0,0 +1,199 @@
+const selector = document.getElementById("selector");
+const octave = document.getElementById("octave");
+const snap = document.getElementById("snap");
+const aref = 440; // source A reference
+
+let playing = -1; // currently playing _midi note_
+let playingKey = ""; // currently playing key code
+let access; // midi access
+
+const INTERVALS = [1, 2, 3/2, 5/4, 7/4, 11/4, 13/8];
+
+//const realbase = 261.63/8;
+const realbase = 289.56/8; // yxeni lol
+const horiz = 2;
+const vert = Number(window.location.search.slice(1));
+
+const UP = "ꜛ";
+const DOWN = "ꜜ";
+
+// e.code is used here instead of e.key so it doesn't depend on your layout
+const keys = {
+  "Digit1": [-5, 2],
+  "Digit2": [-4, 2],
+  "Digit3": [-3, 2],
+  "Digit4": [-2, 2],
+  "Digit5": [-1, 2],
+  "Digit6": [0, 2],
+  "Digit7": [1, 2],
+  "Digit8": [2, 2],
+  "Digit9": [3, 2],
+  "Digit0": [4, 2],
+
+  "KeyQ": [-5, 1],
+  "KeyW": [-4, 1],
+  "KeyE": [-3, 1],
+  "KeyR": [-2, 1],
+  "KeyT": [-1, 1],
+  "KeyY": [0, 1],
+  "KeyU": [1, 1],
+  "KeyI": [2, 1],
+  "KeyO": [3, 1],
+  "KeyP": [4, 1],
+
+  "KeyA": [-5, 0],
+  "KeyS": [-4, 0],
+  "KeyD": [-3, 0],
+  "KeyF": [-2, 0],
+  "KeyG": [-1, 0],
+  "KeyH": [0, 0],
+  "KeyJ": [1, 0],
+  "KeyK": [2, 0],
+  "KeyL": [3, 0],
+  "Semicolon": [4, 0],
+
+  "KeyZ": [-5, -1],
+  "KeyX": [-4, -1],
+  "KeyC": [-3, -1],
+  "KeyV": [-2, -1],
+  "KeyB": [-1, -1],
+  "KeyN": [0, -1],
+  "KeyM": [1, -1],
+  "Comma": [2, -1],
+  "Period": [3, -1],
+  "Slash": [4, -1],
+};
+
+// thanks mozilla
+function getBaseLog(x, y) {
+  return Math.log(y) / Math.log(x);
+}
+
+function connect() {
+  navigator.requestMIDIAccess().then((a) => {
+    access = a;
+    let selectorData = "";
+    access.outputs.forEach((port, id) => {
+      selectorData += `<option value="${id}">${port.name}</option>`;
+    });
+    selector.innerHTML = selectorData;
+  });
+}
+
+function send(pitch) {
+  const output = access.outputs.get(selector.value);
+  
+  if (playing !== -1)
+    stop();
+
+  const note = 12 * getBaseLog(2, pitch / aref) + 69;
+  const roundNote = Math.round(note);
+  const roundFreq = 2**((roundNote-69)/12)*aref;
+  const difference = 12 * getBaseLog(2, pitch / roundFreq);
+  const fuckYouMidi = difference * 32;
+  console.log(pitch, difference);
+
+  playing = roundNote;
+  output.send([0x90, playing, 0x7f, // note on
+               0xE0, 0, Math.round(64 + fuckYouMidi)]); // pitch bend
+}
+
+function stop() {
+  const output = access.outputs.get(selector.value);
+  if (playing !== -1) {
+    output.send([0x80, playing, 0x7f]);
+    playing = -1;
+  }
+}
+
+function getBox(pos) {
+  return document.querySelector(`tr.t${pos[1]}>td.t${pos[0]}`);
+}
+
+function keydown(e) {
+  console.log(e.code);
+  // ignore hotkeys such as ctrl+r to reload
+  if (e.altKey || e.ctrlKey || e.metaKey)
+    return;
+  
+  // special handling to change octaves
+  if (e.code=="ArrowLeft")
+    octave.value = Number(octave.value) - 1;
+  if (e.code=="ArrowRight")
+    octave.value = Number(octave.value) + 1;
+
+  // do nothing if the midi access isn't there
+  if (!access)
+    return;
+  // ignore keys that aren't in the playing area
+  if (!(e.code in keys))
+    return;
+
+  playingKey = e.code;
+  const pos = keys[e.code];
+  const base = realbase * (2 ** octave.value);
+  let pitch = base * (INTERVALS[horiz] ** pos[0]) * (INTERVALS[vert] ** pos[1]);
+
+  // snap to octave (please tell me there's a better way to do this)
+  if (snap.checked) {
+    while (pitch > base * 2)
+      pitch /= 2;
+    while (pitch < base)
+      pitch *= 2;
+  }
+
+  getBox(pos).classList.add("selected");
+  send(pitch);
+}
+
+function keyup(e) {
+  // do nothing if the midi access isn't there
+  if (!access)
+    return;
+  // ignore hotkeys such as ctrl+r to reload
+  if (e.altKey || e.ctrlKey || e.metaKey)
+    return;
+  // ignore keys that aren't in the playing area
+  if (!(e.code in keys))
+    return;
+
+  const pos = keys[e.code];
+  getBox(pos).classList.remove("selected");
+
+  if (playingKey == e.code)
+    stop();
+}
+
+function addnames() {
+  for (let x = -5; x <= 4; x++) {
+    for (let y = -1; y <= 2; y++) {
+      const newline = document.createElement("br");
+      const box = getBox([x, y]);
+      box.appendChild(newline);
+
+      if (x == 0 && y == 0) {
+        const span = document.createElement("span");
+        span.innerText = "0";
+        span.classList.add("qoz", "color0");
+        box.appendChild(span);
+      } else {
+        if (x != 0) {
+          const horizSpan = document.createElement("span");
+          horizSpan.innerText += horiz + (x < 0 ? DOWN : UP).repeat(Math.abs(x));
+          horizSpan.classList.add("qoz", `color${horiz}`);
+          box.appendChild(horizSpan);
+        }
+        if (y != 0) {
+          const vertSpan = document.createElement("span");
+          vertSpan.innerText += vert + (y < 0 ? DOWN : UP).repeat(Math.abs(y));
+          vertSpan.classList.add("qoz", `color${vert}`);
+          box.appendChild(vertSpan);
+        }
+      }
+    }
+  }
+}
+
+addnames();
+document.body.addEventListener("keydown", keydown);
+document.body.addEventListener("keyup", keyup);
